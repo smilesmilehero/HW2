27848dd333c87dc5d390205c632fc834a4dc4f22
smilesmilehero <jordy827@yahoo.com.tw>
2018-10-08 08:59:42 +0800

    Project init

---
+++ test_p28/src/sample2_12/sample2_12.java
@@ -0,0 +1,13 @@
+package sample2_12;
+
+public class sample2_12 {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println("第"+i+"次的迴圈");
+			i++;
+		}while(i<=5);
+	System.out.println("迴圈結束");
+	} 
+}
---
+++ test_p34/src/sample2_15/sample2_15.java
@@ -0,0 +1,26 @@
+package sample2_15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sample2_15 {
+
+	public static void main(String[] args) throws IOException 
+	{
+		System.out.println("要跳過第幾次的處裡呢?(1~10)");
+		
+		BufferedReader br =
+				new BufferedReader(new InputStreamReader(System.in));
+	
+		String str=br.readLine();
+		int res = Integer.parseInt(str);
+		
+		for(int i=1;i<=10;i++) {
+			if(i==res)
+				continue;
+			System.out.println("第"+i+"次的處理。");
+		}
+	}
+
+}
